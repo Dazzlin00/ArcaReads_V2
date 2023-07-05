820cfb1fd3a9002b9ac519689100ace2d393b9a3
Dazzlin00 <eliza5478@gmail.com>
2023-07-05 13:09:34 -0400

    Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,101 +1,124 @@
-import React, { useState } from "react";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ModeCommentRoundedIcon from '@mui/icons-material/ModeCommentRounded';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Badge from '@mui/material/Badge';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 
-import {
-  Share,
-} from "react-native";
-
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(0deg)',
-  marginLeft: '0px',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-
-export default function Post({ post, onLike, onDislike }) {
-  const [comment, setComment] = useState("");
-
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  const handleShare = async () => {
-    try {
-      await Share.share({
-        message: `Mira este post: ${post.title} - ${post.content}`,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const handleComment = () => {
-    // Aquí puedes agregar la lógica para guardar el comentario en la base de datos o en el estado de la aplicación.
-    console.log(comment);
-    setComment("");
-  };
-
-
+const Post = ({ username, timestamp, text, image, postImage }) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-    <CardHeader
-      avatar={
-        <Avatar src={post.avatar} aria-label="recipe">
-          R
-        </Avatar>
-      }
-      action={
-        <IconButton aria-label="settings">
-          <MoreVertIcon />
-        </IconButton>
-      }
-      title={post.name}
-      subheader={post.createdAt}
-    />
-    <CardMedia
-      component="img"
-      height="194"
-      image={post.image}
-      alt=""
-    />
-    <CardActions disableSpacing >
-      <Badge badgeContent={post.likes} color="primary" >
-        <FavoriteIcon color="action"/>
-      </Badge>
+    <Card containerStyle={styles.card}>
+      <View style={styles.header}>
+        
+        <Image
+          source={require("../../assets/panda.jpg")}
+          style={styles.avatar}
+        />
 
-      <Badge sx={{ml: 1.5}} badgeContent={post.commnents} color="secondary">
-        <ModeCommentRoundedIcon color="action" />
-      </Badge>
-    </CardActions>
+        <Text style={styles.username}>Eliza Graterol</Text>
+      </View>
+      <Text style={styles.text}>Hola Mundo</Text>
+    
+      { 
+      (
+        <Image source={require("../../assets/panda.jpg")}
+        style={styles.postImage} />
+      )}
 
-    <CardContent >
-      <Typography sx={{ mb: 0, mt:-3 }}  variant="subtitle2"  display="block" gutterBottom>
-       {post.content}
-      </Typography>
-    </CardContent>
+      <View style={styles.footer}>
+        <View style={styles.footerButton}>
+          <Button
+            icon={<Icon name="thumb-up" size={20} color="#3b5998" />}
+            title="Like"
+            titleStyle={styles.buttonTitle}
+            type="clear"
+          />
+          <Text style={styles.buttonCount}>500</Text>
+        </View>
+       
+        <View style={styles.footerButton}>
+          <Button
+            icon={<Icon name="comment" size={20} color="#3b5998" />}
+            title="Comment"
+            titleStyle={styles.buttonTitle}
+            type="clear"
+          />
+          <Text style={styles.buttonCount}>50</Text>
+        </View>
 
-  </Card>
 
-
+        <View style={styles.footerButton}>
+          <Button
+            icon={<Icon name="share" size={20} color="#3b5998" />}
+            title="Share"
+            titleStyle={styles.buttonTitle}
+            type="clear"
+          />
+        </View>
+      </View>
+      <Text style={styles.timestamp}>{timestamp}</Text>
+    </Card>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  username: {
+    marginLeft: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  text: {
+    marginHorizontal: 10,
+    marginBottom: 10,
+    fontSize: 16,
+  },
+  postImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+    resizeMode: 'cover',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginBottom: 10,
+  },
+  footerButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    color: '#3b5998',
+    marginLeft: 5,
+  },
+  buttonCount: {
+    marginLeft: 5,
+    color: 'gray',
+  },
+  timestamp: {
+    color: 'gray',
+    fontSize: 12,
+    marginHorizontal: 10,
+    marginBottom: 5,
+  },
+});
+
+export default Post;
