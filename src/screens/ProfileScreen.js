1ec88ffd002f558fe1dc5de49547bdbaa29f411c
Elizabeth Gomez <elizabethgomez.ucla@gmail.com>
2023-07-08 21:20:03 -0400

    primer avance

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons';
 import Profile from '../components/Profile';
 
+
+import { View, ScrollView, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 export default function ProfileScreen() {
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header }>
+        <Image source={require('../../assets/logoblanco.png')} style={styles.logo} resizeMode="contain" />
+        <TouchableOpacity style={styles.notificationButton}>
+          <Ionicons name="settings-outline" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
       <Profile />
-    </View>
+     </View>
+     
+
+    </SafeAreaView>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginTop: 20 ,
+    height: 60,
+    backgroundColor: '#4D194D',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  notificationButton: {
+    marginRight: 10,
+  },
+  logo: {
+    width: 150,
+    height: 50,
+  },
+});
