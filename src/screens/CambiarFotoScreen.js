7dc3120524a942245752d4caf1fb4a771d3d2c5a
Elizabeth Gomez <elizabethgomez.ucla@gmail.com>
2023-07-10 01:40:32 -0400

    cambio en las barras

--- src/screens/CambiarFotoScreen.js
+++ src/screens/CambiarFotoScreen.js
@@ -1,42 +1,69 @@
 import React from 'react';
 import { Ionicons } from 'react-native-vector-icons';
 import CambiarFoto from '../components/CambiarFoto';
-
-import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { View, Text, Image, StyleSheet, FlatList,SafeAreaView, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
 export default function CambiarFotoScreen() {
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require('../../assets/logoblanco.png')} style={styles.logo} resizeMode="contain" />
-        <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={styles.notificationButton}>
-          <Ionicons name="settings-outline" size={30} color="white" />
-        </TouchableOpacity>
+    <SafeAreaView style={styles.Container}>
+      
+    <LinearGradient
+      colors={["rgba(238,174,202,0.7)", "rgba(93,135,218,0.9)"]}
+      start={{ x: 1, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={styles.logocontainer}
+    >
+      <View style={styles.logoWrapper}>
+        <Image
+          source={require("../../assets/logoblanco.png")}
+          style={styles.imagelogo}
+        />
       </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Settings")}
+        style={styles.notificationButton}
+      >
+      <Ionicons name="settings-outline" size={30} color="white" />
+      </TouchableOpacity>
+    </LinearGradient>
 
-      <CambiarFoto />
+
+    <CambiarFoto/>
     
-    </View>
+
+</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
     marginTop: 20,
-    height: 60,
-    backgroundColor: '#4D194D',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 10,
+
+    
+    backgroundColor: "white",
   },
+  logocontainer: {
+    height: 80,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logoWrapper: {
+    width: 160,
+  },
+  imagelogo: {
+    resizeMode: "contain",
+    margin: 20,
+
+    width: "100%",
+    height: "100%",
+  },
+ 
   notificationButton: {
     marginRight: 10,
   },
